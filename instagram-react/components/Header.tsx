3f0fdf10fe4a4929c483ff6cbe6e67d1d0449474
TWasilonek <tomasz.wasilonek@gmail.com>
2022-08-01 19:09:38 +0200

    top bar search

--- instagram-react/components/Header.tsx
+++ instagram-react/components/Header.tsx
@@ -1,30 +1,40 @@
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <div>
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            src="/images/instagram_logo.png"
-            alt="Instagram logo"
-            className="object-contain"
-            layout="fill"
-          />
-        </div>
-        <div className="cursor-pointer h-24 w-6 relative lg:hidden">
-          <Image
-            src="/images/instagram_logo_icon.png"
-            alt="Instagram logo"
-            className="object-contain"
-            layout="fill"
-          />
-        </div>
-        <div>Right side</div>
+    <div className="flex items-center justify-between max-w-6xl">
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          src="/images/instagram_logo.png"
+          alt="Instagram logo"
+          className="object-contain"
+          layout="fill"
+        />
+      </div>
+      <div className="cursor-pointer h-24 w-6 relative lg:hidden">
+        <Image
+          src="/images/instagram_logo_icon.png"
+          alt="Instagram logo"
+          className="object-contain"
+          layout="fill"
+        />
       </div>
 
       {/* Middle */}
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <SearchIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        />
+      </div>
+
       {/* Right */}
+      <div>Right side</div>
     </div>
   );
 };
