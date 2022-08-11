0a780513bf40f509b477a14e46bb26fef64f14d5
TWasilonek <tomasz.wasilonek@gmail.com>
2022-08-11 18:23:41 +0200

    finished elements in the Post

--- instagram-react/src/components/Post.tsx
+++ instagram-react/src/components/Post.tsx
@@ -1,17 +1,17 @@
 import { FC } from "react";
 import Image from "next/image";
-import { DotsHorizontalIcon } from "@heroicons/react/outline";
+import {
+  DotsHorizontalIcon,
+  HeartIcon,
+  ChatIcon,
+  BookmarkIcon,
+  EmojiHappyIcon,
+} from "@heroicons/react/outline";
 import { PostType } from "../types";
 
 type PostProps = PostType & {};
 
-const StoryThumbnail: FC<PostProps> = ({
-  id,
-  img,
-  username,
-  userImg,
-  caption,
-}) => {
+const Post: FC<PostProps> = ({ id, img, username, userImg, caption }) => {
   return (
     <div className="bg-white my-7 border ronded-md">
       <div className="flex items-center p-5">
@@ -32,8 +32,33 @@ const StoryThumbnail: FC<PostProps> = ({
         height={470}
         className="object-cover object-center"
       />
+
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
+
+      <p className="p-5 truncate">
+        <span className="font-bold mr-2">{username}</span>
+        {caption}
+      </p>
+
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          type="text"
+          placeholder="Enter your comment..."
+          className="border-none flex-1"
+        />
+        <button type="submit" className="text-blue-400 font-bold">
+          Post
+        </button>
+      </form>
     </div>
   );
 };
 
-export default StoryThumbnail;
+export default Post;
